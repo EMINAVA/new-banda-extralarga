be61efab459837fe5249aa6a5d72fbeda443363b
fra <francesco.genna03@gmail.com>
2022-03-20 13:00:00 +0100

    Update Roles.svelte

--- src/pages/roles/Roles.svelte
+++ src/pages/roles/Roles.svelte
@@ -63,7 +63,7 @@
         </span>
     </div>
     <div
-        class="card order-4 bg-white dark:bg-navbar-background-dark col-span-3 xl:col-span-6 row-span-2 flex flex-col px-6"
+        class="card order-4 bg-white dark:bg-navbar-background-dark col-span-3 xl:col-span-4 row-span-2 flex flex-col px-6"
     >
         <div class="grid grid-cols-7 items-center">
             <div class="flex">
@@ -105,67 +105,24 @@
         </ul>
     </div>
     <div
-        class="card order-3 bg-white dark:bg-navbar-background-dark col-span-3 xl:col-span-3 row-span-1"
-    >
+        class="card order-3 bg-white dark:bg-navbar-background-dark col-span-3 xl:col-span-3 row-span-1">
+
         <!-- svelte-ignore a11y-media-has-caption -->
         <video class="h-96 m-auto" height="400" controls>
             <source type="video/mp4" src="/img/intro_video.mp4" />
         </video>
+
+    </div>
+
+    <div
+    class="card order-5 bg-white dark:bg-navbar-background-dark col-span-3 xl:col-span-6 row-span-2">
+
+        <iframe width="640" height="450" class="m-auto" src="https://www.youtube.com/embed/rkhVsPsE6DA" title="YouTube video player" frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
     </div>
 </div>
 
-<!---
-  <div class="container mx-auto px-4">
-    <span class="block mb-1">
-      Il progetto banda ultra larga ha lo scopo di servire l'intero territorio italiano di connessione a banda
-      ultra
-      larga nelle zone dove ancora non è presentel Piano nazionale per la banda ultralarga prevede un intervento
-      pubblico che investe il territorio nazionale distinguendo tra:
-    </span>
-    <ul class="list-disc list-inside mb-3">
-      <li>
-        Le aree cd. ‘bianche’, a fallimento di mercato, in cui sono assenti interventi di investimento di
-        operatori
-        privati;
-      </li>
-      <li>
-        Le aree cd. ‘grigie’ e ‘nere’ ovvero in concorrenza, ove sono già presenti una o più reti in banda
-        ultralarga,
-        al fine di conseguire, anche in tali aree, un importante salto di qualità per la realizzazione di reti
-        in banda
-        ultraveloce al gigabit.
-      </li>
-    </ul>
-    <span class="block mb-1">
-      Il Piano pertanto si articola in due fasi. La prima riguarda l’attuazione di misure a sostegno
-      dell’infrastrutturazione delle aree bianche.
-      A seguire, nella seconda fase, verranno implementati gli strumenti a favore dello sviluppo di reti
-      ultraveloci
-      nelle aree nere e grigie del Paese.
-    </span>
-    <span class="block mb-1">
-      Nel nostro progetto ci siamo suddivisi nei vari ruoli:
-    </span>
-    <ul class="list-disc list-inside mb-2">
-      <li class="">Tesainer-project menager: si occupa di organizzare il lavoro nel gruppo e presentarlo;</li>
-      <li>Nava e Genna-coder: si occupano della parte di programmazione del gruppo, programmando le applicazioni
-        necessarie al gruppo;
-      </li>
-      <li>Parlabene e Caponeri-analista: raccoglie informazioni da varie fonti analitiche;</li>
-      <li>De Chiara-scout: reperisce le fonti di informazione;</li>
-      <li>Nedelcu-storyteller: presenta il progetto una volta finito;</li>
-      <li>Bertani-social media manager: si occupa di tutti i social del progetto;</li>
-      <li>Macchiarulo-blogger: si occupa di inserire le informazioni sui vari social;</li>
-      <li>Bortolami-designer: si occupa delle parti grafiche del progetto;</li>
-    </ul>
-    <div class="flex flex-wrap mt-2">
-      <img class="h-96" src="/img/mappa.jpg" alt="Mappa Lombardia">
-      <video class="h-96 m-auto" height="400" controls>
-        <source type="video/mp4" src="/img/intro_video.mp4">
-      </video>
-    </div>
-  </div>
-</div>-->
 <style lang="postcss">
     @tailwind components;
 
