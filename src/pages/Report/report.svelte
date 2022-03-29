3b02357b58bad8afae083249831a8714718f4c1a
EMINAVA <emiliano230104@gmail.com>
2022-03-29 23:35:38 +0200

    Aggiunto video

--- src/pages/Report/report.svelte
+++ src/pages/Report/report.svelte
@@ -2,52 +2,70 @@
     import "@carbon/charts/styles.min.css";
     import "carbon-components/css/carbon-components.min.css";
     import MdInfoOutline from "svelte-icons/md/MdInfoOutline.svelte";
+    import MdVideocam from 'svelte-icons/md/MdVideocam.svelte'
 </script>
 
 <svelte:head><title>Report</title></svelte:head>
 
-<div class="font-bold text-center text-5xl mb-6 my-0">REPORT</div>
-
-<div class="grid col-start-2 grid-cols-3 center md:grid-cols-6 3xl:grid-cols-10 gap-6">
-  <div
-    class="card order-1 bg-white dark:bg-navbar-background-dark xl:col-start-2 xl:col-end-10 col-span-8 row-span-2 flex flex-col px-6">
-    <div class="grid grid-cols-7 items-center justify-between">
-      <div class="flex">
-        <span class="h-10 flex items-start"><MdInfoOutline/></span>
+<div>
+  <div class="font-bold text-center text-5xl mb-6 my-0">REPORT</div>
+  <div class="grid grid-cols-3 center md:grid-cols-6 3xl:grid-cols-10 gap-6">
+    <div
+      class="card order-1 bg-white dark:bg-navbar-background-dark col-span-10 xl:col-span-5 flex flex-col px-6 row-span-2">
+      <div class="grid grid-cols-7 items-center justify-between">
+        <div class="flex">
+          <span class="h-10 flex items-start"><MdInfoOutline/></span>
+        </div>
+        <span class="w-full text-center font-bold text-2xl col-span-5">REPORT PROGETTO BANDA ULTRA LARGA</span>
       </div>
-      <span class="w-full text-center font-bold text-2xl col-span-5">REPORT PROGETTO BANDA ULTRA LARGA</span>
+      <p class="flex-grow mt-5">
+        Lo scopo del progetto banda ultra larga è diffondere in tutta la Lombardia una connessione in fibra ottica di
+        almeno 100 mb/s,
+        noi ci stiamo occupando di seguire l'andamento del progetto.
+        All'inizio del programma il blogger si è occupato di scrivere un testo in cui presentava il nostro lavoro e le
+        nostre intenzioni,
+        spiegando anche brevemente i nostri compiti;
+        invece gli analisti si sono occupati di ideare un'intervista in cui rispondevano a delle ipotetiche domande
+        fatte
+        da una persona esterna sul progetto.
+        Mentre lo scout e gli analisti cercavano informazioni per ampliare il piano, i coder hanno iniziato a creare il
+        sito,
+        aiutati dal blogger e il social media manager che sceglievano le informazioni più pertinenti da mettere nella
+        home.
+        Successivamente il social media manager e lo storyteller hanno registrato un video presentazione di tutti i
+        componenti della squadra;
+        con questo è finita la prima fase del progetto.
+        Nella seconda fase lo scopo principale era redigere un articolo di data journalism a partire dai dati trovati
+        nella prima fase.
+        Il blogger e il social media manager si sono occupati di organizzare e documentare il lavoro del team,
+        raccontando
+        gli avanzamenti della ricerca;
+        lo storyteller e il designer hanno verificato che i grafici e le infografiche realizzate nell'articolo di Data
+        Journalism fossero efficaci a livello comunicativo.
+        Il blogger e il social media manager si sono occupati anche di postare tutti i contenuti sui social.
+        Per concludere la seconda fase abbiamo fatto dei video individuali in cui ognuno spiegava il proprio ruolo nel
+        gruppo.
+        Nella terza fase il project manager ha organizzato come sempre un piano di lavoro e delle scadenze per tutto il
+        gruppo, invece lo storyteller,
+        il designer ed i coder hanno realizzato un video in cui presentavano il sito;
+        il blogger e il social media manager hanno diffuso sui social il report di monitoraggio realizzato su <a
+        href="https://www.monithon.eu/it/" class="text-blue-600">Monithon</a>.
+      </p>
     </div>
-    <p class="flex-grow mt-5">
-      Lo scopo del progetto banda ultra larga è diffondere in tutta la Lombardia una connessione in fibra ottica di
-      almeno 100 mb/s,
-      noi ci stiamo occupando di seguire l'andamento del progetto.
-      All'inizio del programma il blogger si è occupato di scrivere un testo in cui presentava il nostro lavoro e le
-      nostre intenzioni,
-      spiegando anche brevemente i nostri compiti;
-      invece gli analisti si sono occupati di ideare un'intervista in cui rispondevano a delle ipotetiche domande fatte
-      da una persona esterna sul progetto.
-      Mentre lo scout e gli analisti cercavano informazioni per ampliare il piano, i coder hanno iniziato a creare il
-      sito,
-      aiutati dal blogger e il social media manager che sceglievano le informazioni più pertinenti da mettere nella
-      home.
-      Successivamente il social media manager e lo storyteller hanno registrato un video presentazione di tutti i
-      componenti della squadra;
-      con questo è finita la prima fase del progetto.
-      Nella seconda fase lo scopo principale era redigere un articolo di data journalism a partire dai dati trovati
-      nella prima fase.
-      Il blogger e il social media manager si sono occupati di organizzare e documentare il lavoro del team, raccontando
-      gli avanzamenti della ricerca;
-      lo storyteller e il designer hanno verificato che i grafici e le infografiche realizzate nell'articolo di Data
-      Journalism fossero efficaci a livello comunicativo.
-      Il blogger e il social media manager si sono occupati anche di postare tutti i contenuti sui social.
-      Per concludere la seconda fase abbiamo fatto dei video individuali in cui ognuno spiegava il proprio ruolo nel
-      gruppo.
-      Nella terza fase il project manager ha organizzato come sempre un piano di lavoro e delle scadenze per tutto il
-      gruppo, invece lo storyteller,
-      il designer ed i coder hanno realizzato un video in cui presentavano il sito;
-      il blogger e il social media manager hanno diffuso sui social il report di monitoraggio realizzato su <a
-      href="https://www.monithon.eu/it/" class="text-blue-600">Monithon</a>.
-    </p>
+    <div
+      class="card order-1 bg-white dark:bg-navbar-background-dark col-span-10 xl:col-span-5 flex flex-col justify-start px-6">
+      <div class="grid grid-cols-7 items-center justify-between mb-10">
+        <div class="flex">
+          <span class="h-10 flex items-start"><MdVideocam/></span>
+        </div>
+        <span class="w-full text-center font-bold text-2xl col-span-5">VIDEO REPORT</span>
+      </div>
+      <iframe src="https://www.youtube.com/embed/7ssVU16Kdf4" title="YouTube video player"
+              class="w-full rounded-lg h-80"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen></iframe>
+    </div>
   </div>
 </div>
 
--- src/pages/roles/Roles.svelte
+++ src/pages/roles/Roles.svelte
@@ -9,51 +9,51 @@
 
 <svelte:head><title>Ruoli</title></svelte:head>
 <div>
-    <div class="font-bold text-center text-5xl mb-6 my-0">BANDA ULTRA LARGA</div>
-    <div class="grid grid-cols-3 md:grid-cols-6 3xl:grid-cols-10 gap-6">
-        <div
-                class="card order-first bg-white dark:bg-navbar-background-dark col-span-3 xl:col-span-4 row-span-1 flex flex-col px-6"
+  <div class="font-bold text-center text-5xl mb-6 my-0">BANDA ULTRA LARGA</div>
+  <div class="grid grid-cols-3 md:grid-cols-6 3xl:grid-cols-10 gap-6">
+    <div
+      class="card order-first bg-white dark:bg-navbar-background-dark col-span-3 xl:col-span-4 row-span-1 flex flex-col px-6"
+    >
+      <div class="grid grid-cols-7 items-center justify-between">
+        <div class="flex">
+          <span class="h-10 flex items-start"><MdInfo/></span>
+        </div>
+        <span class="w-full text-center font-bold text-2xl col-span-5"
+        >Progetto</span
         >
-            <div class="grid grid-cols-7 items-center justify-between">
-                <div class="flex">
-                    <span class="h-10 flex items-start"><MdInfo/></span>
-                </div>
-                <span class="w-full text-center font-bold text-2xl col-span-5"
-                >Progetto</span
-                >
-            </div>
-            <span class="block mb-1 flex-grow mt-5">
+      </div>
+      <span class="block mb-1 flex-grow mt-5">
             Il progetto banda ultra larga ha lo scopo di servire l'intero
             territorio italiano di connessione a banda ultra larga nelle zone
             dove ancora non è presente. Il Piano nazionale per la banda ultralarga
             prevede un intervento pubblico che investe il territorio nazionale
             distinguendo tra:
         </span>
-            <ul class="list-disc list-inside mb-3">
-                <li>
-                    Le aree cd. ‘bianche’, a fallimento di mercato, in cui sono
-                    assenti interventi di investimento di operatori privati;
-                </li>
-                <li>
-                    Le aree cd. ‘grigie’ e ‘nere’ ovvero in concorrenza, ove sono
-                    già presenti una o più reti in banda ultralarga, al fine di
-                    conseguire, anche in tali aree, un importante salto di qualità
-                    per la realizzazione di reti in banda ultraveloce al gigabit.
-                </li>
-            </ul>
+      <ul class="list-disc list-inside mb-3">
+        <li>
+          Le aree cd. ‘bianche’, a fallimento di mercato, in cui sono
+          assenti interventi di investimento di operatori privati;
+        </li>
+        <li>
+          Le aree cd. ‘grigie’ e ‘nere’ ovvero in concorrenza, ove sono
+          già presenti una o più reti in banda ultralarga, al fine di
+          conseguire, anche in tali aree, un importante salto di qualità
+          per la realizzazione di reti in banda ultraveloce al gigabit.
+        </li>
+      </ul>
+    </div>
+    <div
+      class="card order-2 bg-white dark:bg-navbar-background-dark col-span-3 xl:col-span-3 row-span-1 flex flex-col px-6"
+    >
+      <div class="grid grid-cols-3 items-center justify-between">
+        <div class="flex">
+          <span class="h-10 flex items-start"><MdDescription/></span>
         </div>
-        <div
-                class="card order-2 bg-white dark:bg-navbar-background-dark col-span-3 xl:col-span-3 row-span-1 flex flex-col px-6"
+        <span class="w-full text-center font-bold text-2xl "
+        >Descrizione</span
         >
-            <div class="grid grid-cols-3 items-center justify-between">
-                <div class="flex">
-                    <span class="h-10 flex items-start"><MdDescription/></span>
-                </div>
-                <span class="w-full text-center font-bold text-2xl "
-                >Descrizione</span
-                >
-            </div>
-            <span class="flex-grow mt-5">
+      </div>
+      <span class="flex-grow mt-5">
             Il progetto “Grande Progetto Banda Ultra Larga” ha lo scopo di
             portare una connessione a banda ultra larga in tutte le zone bianche
             della Lombardia. Una zona bianca è una zona dove l’investimento da
@@ -61,69 +61,69 @@
             conveniente, e quindi le connessioni rimangono quelle ADSL che si
             usavano nel passato.
         </span>
+    </div>
+    <div
+      class="card order-4 bg-white dark:bg-navbar-background-dark col-span-3 xl:col-span-4 row-span-2 flex flex-col px-6"
+    >
+      <div class="grid grid-cols-7 items-center">
+        <div class="flex">
+          <span class="h-10 flex items-start"><MdPerson/></span>
         </div>
-        <div
-                class="card order-4 bg-white dark:bg-navbar-background-dark col-span-3 xl:col-span-4 row-span-2 flex flex-col px-6"
+        <span class="w-full text-center font-bold text-2xl col-span-5"
+        >Chi siamo?</span
         >
-            <div class="grid grid-cols-7 items-center">
-                <div class="flex">
-                    <span class="h-10 flex items-start"><MdPerson/></span>
-                </div>
-                <span class="w-full text-center font-bold text-2xl col-span-5"
-                >Chi siamo?</span
-                >
-            </div>
-            <span class="flex-grow mt-5">
+      </div>
+      <span class="flex-grow mt-5">
             Nel nostro progetto ci siamo suddivisi nei vari ruoli:
         </span>
-            <ul class="list-disc list-inside mb-2">
-                <li class="">
-                    Tesainer-project menager: si occupa di organizzare il lavoro nel
-                    gruppo e presentarlo;
-                </li>
-                <li>
-                    Nava e Genna-coder: si occupano della parte di programmazione
-                    del gruppo, programmando le applicazioni necessarie al gruppo;
-                </li>
-                <li>
-                    Parlabene e Caponeri-analista: raccoglie informazioni da varie
-                    fonti analitiche;
-                </li>
-                <li>De Chiara-scout: reperisce le fonti di informazione;</li>
-                <li>Nedelcu-storyteller: presenta il progetto una volta finito;</li>
-                <li>
-                    Bertani-social media manager: si occupa di tutti i social del
-                    progetto;
-                </li>
-                <li>
-                    Macchiarulo-blogger: si occupa di inserire le informazioni sui
-                    vari social;
-                </li>
-                <li>
-                    Bortolami-designer: si occupa delle parti grafiche del progetto;
-                </li>
-            </ul>
-        </div>
-        <div
-                class="card order-3 bg-white dark:bg-navbar-background-dark col-span-3 xl:col-span-3 row-span-1">
-
-            <!-- svelte-ignore a11y-media-has-caption -->
-            <video class="h-96 m-auto rounded-lg" height="400" controls>
-                <source type="video/mp4" src="/img/intro_video.mp4"/>
-            </video>
-
-        </div>
-
-        <div
-                class="card order-5 bg-white dark:bg-navbar-background-dark col-span-3 xl:col-span-6 row-span-2">
-
-            <iframe width="640" height="450" class="m-auto rounded-lg" src="https://www.youtube.com/embed/rkhVsPsE6DA"
-                    title="YouTube video player" frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen></iframe>
-
-        </div>
+      <ul class="list-disc list-inside mb-2">
+        <li class="">
+          Tesainer-project menager: si occupa di organizzare il lavoro nel
+          gruppo e presentarlo;
+        </li>
+        <li>
+          Nava e Genna-coder: si occupano della parte di programmazione
+          del gruppo, programmando le applicazioni necessarie al gruppo;
+        </li>
+        <li>
+          Parlabene e Caponeri-analista: raccoglie informazioni da varie
+          fonti analitiche;
+        </li>
+        <li>De Chiara-scout: reperisce le fonti di informazione;</li>
+        <li>Nedelcu-storyteller: presenta il progetto una volta finito;</li>
+        <li>
+          Bertani-social media manager: si occupa di tutti i social del
+          progetto;
+        </li>
+        <li>
+          Macchiarulo-blogger: si occupa di inserire le informazioni sui
+          vari social;
+        </li>
+        <li>
+          Bortolami-designer: si occupa delle parti grafiche del progetto;
+        </li>
+      </ul>
     </div>
+    <div
+      class="card order-3 bg-white dark:bg-navbar-background-dark col-span-3 xl:col-span-3 row-span-1">
+
+      <!-- svelte-ignore a11y-media-has-caption -->
+      <video class="h-96 m-auto rounded-lg" height="400" controls>
+        <source type="video/mp4" src="/img/intro_video.mp4"/>
+      </video>
+
+    </div>
+
+    <div
+      class="card order-5 bg-white dark:bg-navbar-background-dark col-span-3 xl:col-span-6 row-span-2">
+
+      <iframe width="640" height="450" class="m-auto rounded-lg" src="https://www.youtube.com/embed/rkhVsPsE6DA"
+              title="YouTube video player" frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen></iframe>
+
+    </div>
+  </div>
 </div>
 
 <style lang="postcss">
