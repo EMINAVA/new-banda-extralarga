cc3f41435a1fd370208dcb144bc4d9969d98d21f
EMINAVA <56084758+EMINAVA@users.noreply.github.com>
2022-04-05 18:58:50 +0200

    Update pages.js

--- src/lib/pages.js
+++ src/lib/pages.js
@@ -3,7 +3,7 @@ import Roles from "../pages/roles/Roles.svelte";
 import Graphs from "../pages/graphs/Graphs.svelte";
 import Coverage from "../pages/coverage/Coverage.svelte";
 import Contacts from "../pages/contacts/Contacts.svelte";
-import Report from "../pages/Report/report.svelte";
+import Report from "../pages/report/report.svelte";
 
 export default [
     { name: "Home", href: "/", component: Home },
